6b81391e50e2dd27aa2af345775dcdc4079c0360
integrii <integrii@gmail.com>
2014-11-26 12:15:24 -0800

    editing readme so the image shows up on chef supermarket

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 ikiwiki CHANGELOG
 =================
 
+1.0.1
+-----
+Updating readme to work on chef supermarket site
+
 1.0.0
 -----
 - Eric Greer - Initial release of ikiwiki
--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'EricGreer@gmail.com'
 license          'All rights reserved'
 description      'Installs ikiwiki and the bootstrap 3 theme'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 depends		 'yum-epel'
